7eea63f7c5496d2f4584f1019db667579d7c5340
redwanchowdhury <redwanc12@gmail.com>
2019-05-09 16:55:44 -1000

    Added new dependencies for rereading pages that render data with javascript. Imported selenium for this.

---
+++ check_avail.py
@@ -0,0 +1,12 @@
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from pathlib import Path
+
+LINK = "https://www12.honolulu.gov/csdarts/frmApptInt.aspx"
+driver = webdriver.Chrome(str(Path().absolute()) + '/chromedriver' )
+driver.get(LINK)
+
+soup = BeautifulSoup(driver.page_source, 'html.parser')
+driver.quit()
+
+print(soup.prettify)
