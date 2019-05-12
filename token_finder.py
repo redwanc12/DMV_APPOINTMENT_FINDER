7ce3fa4c72e12691600503e6fd0ca37a9f0cdc0d
redwanchowdhury <redwanc12@gmail.com>
2019-05-12 00:19:24 -1000

    new functionalties. Can switch pages, months, and more now.

--- token_finder.py
+++ token_finder.py
@@ -19,22 +19,47 @@ class tokenFinder(object):
         self.driver = webdriver.Chrome(str(Path().absolute()) + '/chromedriver' )
         self.driver.set_window_size(1120, 550)
 
-    def scrapeDate(self, date):
+    def authenticateClient(self):
+        # Validates driver. Returns default date.
         self.driver.get(self.url)
         try:
             self.driver.find_element_by_name('btnAcceptTop').click()
+        except NoSuchElementException:
+            pass
+
+        return(self.driver.page_source) 
+
+    def scrapeDate(self, date):
+        # uses web driver to access a new page
+        try:
             self.driver.find_element_by_xpath('//*[@title="{}"]'.format(date)).click()
         except NoSuchElementException:
             pass
-        
-        file = open('nudes2.html', 'w')
-        file.write(self.driver.page_source)
-        file.close()
 
+        return(self.driver.page_source)
+
+    def nextMonth(self):
+        try:
+            self.driver.find_element_by_xpath('//*[@title="Go to the next month"]').click()
+        except NoSuchElementException:
+            pass
+        return(self.driver.page_source)
+
+    def closeDriver(self):
         self.driver.close()
+
+        
 
 
 if __name__ == '__main__':
     scraper = tokenFinder()
-    scraper.scrapeDate("May 24")
+    dateList = ["May 28", "May 29", "June 13"]
+    scraper.authenticateClient()
+    for each in dateList:
+        if('May' not in each):
+            scraper.nextMonth()
+        file = open(each +'.html', 'w')
+        file.write(scraper.scrapeDate(each))
+        file.close()
+    scraper.closeDriver()
 
