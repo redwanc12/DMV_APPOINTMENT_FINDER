1bab95c113b0e56a6a816f7ba9ffe9ad4ecf53e5
redwanchowdhury <redwanc12@gmail.com>
2019-05-12 03:25:21 -1000

    made HTTP handelor object oriented and added new functions.

--- auth_request.py
+++ auth_request.py
@@ -10,33 +10,40 @@ headers = {
     'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36',
     'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3'
 }
-
-def date_id(month, day, year=2019):
+class httpRequester(object):
+    #handles requests through HTTP. No driver needed
+    
+    def __init__(self):
+        self.url = LINK
+        self.headers = headers
+    
+    def date_id(self, month, day, year=2019):
     #Converts month and day to the day ID used by DMV
-    b = 6939 #constant only works after 5/9
-    for each in range(month-1):
-        b += monthrange(year, each+1)[1]
-    b+=day
-    return b
+        b = 6939 #constant only works after 5/9
+        for each in range(month-1):
+            b += monthrange(year, each+1)[1]
+        b+=day
+        return b
 
-def GetAuthorizedPage(dateID, eventVal, viewstate):
+    def GetPage(self, dateID, eventVal, viewstate):
+        #Gets page using dateID, eventVal, viewState
 
-    data = {
-    '__EVENTARGUMENT':dateID,
-    '__EVENTTARGET':'Calendar1',
-    '__EVENTVALIDATION': eventVal,
-    '__VIEWSTATE': viewstate,
-    '__VIEWSTATEENCRYPTED':'',
-    '__VIEWSTATEGENERATOR':'D050787D',
-    'ddlLocation':'0'
-}
+        self.data = {
+        '__EVENTARGUMENT':dateID,
+        '__EVENTTARGET':'Calendar1',
+        '__EVENTVALIDATION': eventVal,
+        '__VIEWSTATE': viewstate,
+        '__VIEWSTATEENCRYPTED':'',
+        '__VIEWSTATEGENERATOR':'D050787D',
+        'ddlLocation':'0'
+        }
 
-    page = requests.post(
-        LINK,
-        data=data,
-        headers=headers,
-    )
-    return page.text
+        page = requests.post(
+            self.url,
+            data=self.data,
+            headers=self.headers,
+        )
+        return page.text
 
 
 # file = open('nudes.html', 'w')
