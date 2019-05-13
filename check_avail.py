7b4bc886878db017533fe344b20588e3951e0714
redwanchowdhury <redwanc12@gmail.com>
2019-05-12 15:07:49 -1000

    made class OOP and cleaned code.

--- check_avail.py
+++ check_avail.py
@@ -2,17 +2,40 @@ from bs4 import BeautifulSoup
 
 locationList = ['Kapalama', 'Kapolei', 'Koolau', 'Wahiawa', 'Waianae']
 
+class openFinder(object):
+    # class to find open dates
+    def __init__(self):
+        self.locationList = locationList
+    
+    def findDates(self, data):
+        soup = BeautifulSoup(data, 'html.parser')
+        times = soup.find_all('tr', {'class':['TableItemLine','TableAltItemLine']})
+        
+        listOfDateDict = []
+
+        for each in times:
+            time = each.td.text
+            spots = each.find_all(['input', 'span'])
+            for index, spot in enumerate(spots):
+                if('None' != spot.text):
+                    dateDict = {
+                        'openings': spot['value'],
+                        'time': time,
+                        'location': locationList[index]
+                    }
+                    listOfDateDict.append(dateDict)
+        return listOfDateDict
+
+
 file = open('test2.html', 'r')
 data = file.read()
 file.close()
 
-soup = BeautifulSoup(data, 'html.parser')
-times = soup.find_all('tr', {'class':['TableItemLine','TableAltItemLine']})
+if __name__ == "__main__":
+    finder = openFinder()
+    dateList = finder.findDates(data)
+    for each in dateList:
+        print(each['openings'] + ' Opening at ' + each['time'] + ' at ' + each['location'])
 
-for each in times:
-    time = each.td.text
-    spots = each.find_all(['input', 'span'])
-    for index, spot in enumerate(spots):
-        if('None' != spot.text):
-            print('Opening at ' + time + ' at ' + locationList[index])
+
 
