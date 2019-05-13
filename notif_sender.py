4b5265e39dfae21dfa83dcfc959ad6cf143876ea
redwanchowdhury <redwanc12@gmail.com>
2019-05-12 23:16:11 -1000

    fixed bugs and added time module. Works enough to be deployed now.

--- notif_sender.py
+++ notif_sender.py
@@ -1,13 +1,23 @@
 import smtplib
 from check_avail import openFinder
 from token_finder import tokenFinder
+import time
+
+emailList = [
+    {
+        'day':'July 01',
+        'email':'redwanc12@gmail.com'
+    },
+    {
+        'day':'August 02',
+        'email':'mchowd13@asu.edu'
+    }
+]
 
 class notif(object):
     def __init__(self):
-        self.email = n/a
-        self.password = n/a
-        self.tf = tokenFinder()
-        self.tf.authenticateClient()
+        self.email = 'dmvnotificationhi@gmail.com'
+        self.password = 'DMVpass123!'
     
     def logIn(self):
         with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
@@ -17,10 +27,14 @@ class notif(object):
 
             smtp.login(self.email, self.password)
 
-    def sendEmail(self, subject, body):
+    def sendEmail(self, email, subject, body):
             with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
+                smtp.ehlo()
+                smtp.starttls()
+                smtp.ehlo()
                 msg = f'Subject: {subject}\n\n{body}'
-                smtp.sendmail(self.email, n/a, msg)
+                smtp.login(self.email, self.password)
+                smtp.sendmail(self.email, email, msg)
 
     def dateToBody(self, dates):
         body = ''
@@ -28,6 +42,21 @@ class notif(object):
             body += (each['openings'] + ' Opening at ' + each['time'] + ' at ' + each['location'] + ' on ' + each['day'] + '\n')
         return body
 
-    def close(self):
-        self.tf.closeDriver()
-    
+
+sender = notif()
+tf = tokenFinder()
+parser = openFinder()
+starttime = time.time()
+
+tf.authenticateClient()
+while(True):
+    tf.refresh()
+    for each in emailList:
+        data = tf.scrapeDate(each['day'])
+        dates = parser.findDates(data)
+        if(dates != []):
+            sender.sendEmail(each['email'], 'Open date', sender.dateToBody(dates))
+
+    time.sleep(60.0 - ((time.time() - starttime) % 60.0))
+
+tf.closeDriver()
