c199fa92212daede7209b89934b149521ba32894
redwanchowdhury <redwanc12@gmail.com>
2019-06-09 13:57:17 -1000

    patched models.

--- models.py
+++ models.py
@@ -2,22 +2,27 @@
 
 class Spots:
     """Spot model"""
-    def __init__(self, openings, time, location, day):
-        self.openings = openings
+    def __init__(self, time, location, day):
         self.time = time
         self.location = location
         self.day = day
 
     def __str__(self):
-        pass
+        return f'opening on {self.day} at {self.time} in {self.location}'
 
 
 class User:
     """User model"""
-    def __init__(self, email, number, excludeList=[]):
+    def __init__(self, email, phone, excludeSpotList=[]):
         self.email = email
-        self.number = number
-        self.excludeList = excludeList
+        self.phone = phone
+        self.excludeSpotList = excludeSpotList
 
     def append_spot(self, spot):
-        self.excludeList.append(spot)
+        self.excludeSpotList.append(spot)
+
+    def __str__(self):
+        return f'email: {self.email} phone: {self.phone} excludeSpotList: {self.excludeSpotList}'
+
+    def number(self):
+        return self.phone
