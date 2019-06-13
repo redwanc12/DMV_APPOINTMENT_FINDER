b2fb399d806e2c1b6463861a6b6e50ef33b69c5c
redwanchowdhury <redwanc12@gmail.com>
2019-06-12 19:56:53 -1000

    fixed function to append exclude spot to customer list.

--- request_handler.py
+++ request_handler.py
@@ -26,3 +26,20 @@ class API:
             headers=self.header
         )
     
+    def append_exclude_list(self, user_id, spot_id):
+        response = requests.get(f'{self.url}api/dmv/customers/{user_id}/', headers=self.header)
+        current_spots = response.json()['excludeSpotList']
+        spot_list = []
+        spot_list.append(current_spots)
+        spot_list.append(spot_id)
+
+        requests.patch(
+            f'{self.url}api/dmv/customers/{user_id}/',
+            {"excludeSpotList":spot_list},
+            headers=self.header)
+    
+headers = {'Authorization':'token 3ab74d348c070ece89b88ea3728f9ed8b22e44b8 '}
+api_test = API('http://127.0.0.1:8000/', headers)
+
+
+api_test.append_exclude_list(8, 23)
